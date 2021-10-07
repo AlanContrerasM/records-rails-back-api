7f0c6e1e768bdf7b723ea289717d4ecaaa07d850
Alan Contreras <alanrcontrerasm@gmail.com>
2021-10-06 23:32:26 -0700

    Create migration, add data, and make it work on localhost

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ApplicationRecord
   #belongs_to :user #might have to delete since tutorial didnt go this far, but works on record
-
+  belongs_to :user, optional: true
   has_many :records, dependent: :destroy
   validates :name, presence: true
 end
